3e1a6c4ca2b17e0ffbbd3bb2c74af8d29a02ae96
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-05-08 21:22:25 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-import("../pkg/index.js").catch(console.error);
+(async function () {
+
+	console.log("[TRACE] <index.js> ### START ###");
+
+	import("../pkg/index.js")
+	.then(function (m) {
+		console.log("[TRACE] WASM module:", m);
+		console.log("[TRACE] Rust uppercase() returned [", m.wasm_to_uppercase("aaaaaAAAaAaAAaA"), "]");
+	})
+	.catch(console.error);
+
+	console.log("[TRACE] <index.js> --- END ---");
+})()
